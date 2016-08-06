03debafaf756176ce77e6ff8f0d3c3977b46dcd1
dani <danihanafi169@gmail.com>
2016-08-06 17:23:11 +0700

    upload first

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,36 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  #btnBack */
+    
+    
+        /* button  #btnBack */
+    
+    
+        /* button  #btnBack */
+    
+    
+        /* button  #btnBack */
+    $(document).on("click", "#btnBack", function(evt)
+    {
+         /*global activate_page */
+         activate_page("#mainpage"); 
+    });
+    
+        /* button  Materi */
+    
+    
+        /* button  Soal */
+    
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
